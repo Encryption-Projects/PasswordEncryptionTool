f22c8e339df790f1de86dad2a80b57d591c27265
coldborne <coldborneVV@gmail.com>
2024-06-24 00:12:51 +0300

    Добавил небольшой интерфейс программы, возможность вводить пароль, соль и путь до файлов вручную пользователем, также добавил обработку исключений и вывод результата шифрования/дешифрования с путём до сохраненного файла, чтобы пользователь знал где его найти

--- PasswordEncryptionTool/Program.cs
+++ PasswordEncryptionTool/Program.cs
@@ -3,51 +3,106 @@ using System.IO;
 using System.Security.Cryptography;
 using System.Text;
 
-class Program
+namespace PasswordEncryptionTool
 {
-    static void Main(string[] args)
+    internal class Program
     {
-        string filePath = "passwords.txt";
-        string encryptedFilePath = "encrypted_passwords.txt";
-        string decryptedFilePath = "decrypted_passwords.txt";
-        string password = "YourStrongPassword123!"; // Пароль для шифрования и дешифрования
-
-        EncryptFile(filePath, encryptedFilePath, password);
-        DecryptFile(encryptedFilePath, decryptedFilePath, password);
-    }
-
-    static void EncryptFile(string inputFilePath, string outputFilePath, string password)
-    {
-        using (Aes aesAlg = Aes.Create())
+        public static void Main(string[] args)
         {
-            var key = new Rfc2898DeriveBytes(password, Encoding.UTF8.GetBytes("Salt1234"), 10000);
-            aesAlg.Key = key.GetBytes(32);
-            aesAlg.IV = key.GetBytes(16);
+            Console.WriteLine(
+                "Добро пожаловать в приложение для шифрования/дешифрования паролей, сохранённых в txt файле");
+            Console.WriteLine("1 - Зашифровать пароли");
+            Console.WriteLine("2 - Дешифровать пароли");
+            Console.WriteLine("3 - Выйти из программы");
 
-            using (var encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV))
-            using (var fileStream = new FileStream(outputFilePath, FileMode.Create))
-            using (var cryptoStream = new CryptoStream(fileStream, encryptor, CryptoStreamMode.Write))
-            using (var inputStream = new FileStream(inputFilePath, FileMode.Open))
+            string userInput = Console.ReadLine();
+
+            Console.Write("Введите путь до txt файла с паролями: ");
+            string filePath = Console.ReadLine();
+
+            Console.Write(
+                "Введите ваш уникальный пароль для шифрования (НИКОМУ ЕГО НЕ ПЕРЕДАВАЙТЕ И НЕ ПОКАЗЫВАЙТЕ): ");
+            string password = Console.ReadLine();
+
+            string projectDirectory = GetProjectDirectory();
+            string salt;
+
+            switch (userInput)
             {
-                inputStream.CopyTo(cryptoStream);
+                case "1":
+                    Console.Write("Введите соль (дополнительная строка для улучшения шифрования): ");
+                    salt = Console.ReadLine();
+                    string encryptedFilePath = Path.Combine(projectDirectory, "encrypted_passwords.txt");
+                    EncryptFile(filePath, encryptedFilePath, password, salt);
+                    break;
+
+                case "2":
+                    Console.Write("Введите соль, которая была использована при шифровании: ");
+                    salt = Console.ReadLine();
+                    string decryptedFilePath = Path.Combine(projectDirectory, "decrypted_passwords.txt");
+                    DecryptFile(filePath, decryptedFilePath, password, salt);
+                    break;
+
+                default:
+                    Console.WriteLine("Такого мы не умеем...");
+                    break;
             }
         }
-    }
 
-    static void DecryptFile(string inputFilePath, string outputFilePath, string password)
-    {
-        using (Aes aesAlg = Aes.Create())
+        public static string GetProjectDirectory()
         {
-            var key = new Rfc2898DeriveBytes(password, Encoding.UTF8.GetBytes("Salt1234"), 10000);
-            aesAlg.Key = key.GetBytes(32);
-            aesAlg.IV = key.GetBytes(16);
+            return Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), @"..\..\"));
+        }
 
-            using (var decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV))
-            using (var fileStream = new FileStream(inputFilePath, FileMode.Open))
-            using (var cryptoStream = new CryptoStream(fileStream, decryptor, CryptoStreamMode.Read))
-            using (var outputStream = new FileStream(outputFilePath, FileMode.Create))
+        public static void EncryptFile(string inputFilePath, string outputFilePath, string password, string salt)
+        {
+            try
             {
-                cryptoStream.CopyTo(outputStream);
+                using (Aes aesAlgorithm = Aes.Create())
+                {
+                    var key = new Rfc2898DeriveBytes(password, Encoding.UTF8.GetBytes(salt), 10000);
+                    aesAlgorithm.Key = key.GetBytes(32);
+                    aesAlgorithm.IV = key.GetBytes(16);
+
+                    using (ICryptoTransform encryptor = aesAlgorithm.CreateEncryptor(aesAlgorithm.Key, aesAlgorithm.IV))
+                    using (FileStream fileStream = new FileStream(outputFilePath, FileMode.Create))
+                    using (CryptoStream cryptoStream = new CryptoStream(fileStream, encryptor, CryptoStreamMode.Write))
+                    using (FileStream inputStream = new FileStream(inputFilePath, FileMode.Open))
+                    {
+                        inputStream.CopyTo(cryptoStream);
+                        Console.WriteLine("Файл успешно зашифрован и сохранён как " + outputFilePath);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Произошла ошибка при шифровании файла: " + ex.Message);
+            }
+        }
+
+        public static void DecryptFile(string inputFilePath, string outputFilePath, string password, string salt)
+        {
+            try
+            {
+                using (Aes aesAlg = Aes.Create())
+                {
+                    var key = new Rfc2898DeriveBytes(password, Encoding.UTF8.GetBytes(salt), 10000);
+                    aesAlg.Key = key.GetBytes(32);
+                    aesAlg.IV = key.GetBytes(16);
+
+                    using (ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV))
+                    using (FileStream fileStream = new FileStream(inputFilePath, FileMode.Open))
+                    using (CryptoStream cryptoStream = new CryptoStream(fileStream, decryptor, CryptoStreamMode.Read))
+                    using (FileStream outputStream = new FileStream(outputFilePath, FileMode.Create))
+                    {
+                        cryptoStream.CopyTo(outputStream);
+                        Console.WriteLine("Файл успешно расшифрован и сохранён как " + outputFilePath);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Произошла ошибка при дешифровании файла: " + ex.Message);
             }
         }
     }
